11db61062dc199d9a484232a9042f935e1c94791
man00154 <149796066+man00154@users.noreply.github.com>
2026-03-25 12:42:27 +0530

    Create loader.py

---
+++ rag/loader.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+
+def load_excel_files(data_path="data"):
+    all_data = []
+
+    for file in os.listdir(data_path):
+        if file.endswith(".xlsx") or file.endswith(".xls"):
+            file_path = os.path.join(data_path, file)
+            df = pd.read_excel(file_path)
+            df['source_file'] = file
+            all_data.append(df)
+
+    combined_df = pd.concat(all_data, ignore_index=True)
+    return combined_df
