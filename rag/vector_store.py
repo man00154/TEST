0c27a390e5fe19b7cc756abe2f1756b2df793aca
man00154 <149796066+man00154@users.noreply.github.com>
2026-03-25 13:10:58 +0530

    Refactor create_documents and vector store creation
    Refactor create_documents function to accept texts instead of a DataFrame and simplify vector store creation.

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -2,18 +2,10 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.docstore.document import Document
 
-
-def create_documents(df):
-    docs = []
-
-    for _, row in df.iterrows():
-        content = " | ".join([f"{col}: {row[col]}" for col in df.columns])
-        docs.append(Document(page_content=content))
-
-    return docs
-
+def create_documents(texts):
+    return [Document(page_content=t) for t in texts]
 
 def create_vector_store(documents):
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    vectorstore = FAISS.from_documents(documents, embeddings)
-    return vectorstore
+    vector_store = FAISS.from_documents(documents, embeddings)
+    return vector_store
