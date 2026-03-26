0121c1daf91ba10e5529d43eac8595df28f1f057
man00154 <149796066+man00154@users.noreply.github.com>
2026-03-26 12:44:14 +0530

    Modify create_documents to handle DataFrame input
    Updated create_documents function to accept a DataFrame and convert its rows into Document objects.

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -3,12 +3,23 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 from langchain_text_splitters import CharacterTextSplitter
 
-def create_documents(texts):
+
+def create_documents(df):
+    documents = []
+
+    # Convert each row into readable text
+    for _, row in df.iterrows():
+        text = " | ".join([f"{col}: {row[col]}" for col in df.columns])
+        documents.append(Document(page_content=text))
+
     splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    return splitter.create_documents(texts)
+
+    return splitter.split_documents(documents)
+
 
 def create_vector_store(documents):
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
+
     return FAISS.from_documents(documents, embeddings)
